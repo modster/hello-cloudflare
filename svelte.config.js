692eb9dd719139c99c7fe5ae93157a590bec802d
E. M. Greeff <unmodster@gmail.com>
2022-01-17 17:57:30 -0700

    installed adapter for cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,10 +9,9 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
-
+		target: '#svelte',
+		adapter: adapter()
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
 	}
 };
 
